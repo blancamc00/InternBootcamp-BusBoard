6cc3358aa7a99c4a6e3690b479d79fc0fdaef7b6
Claire Carlotti <Claire.Calotti@softwire.com>
2022-07-07 15:43:03 +0100

    Nearest bus stop to postcode, attempt at tidying

--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -7,36 +7,68 @@ namespace BusBoard
     {
         static async Task Main(string[] args)
         {
-            //0500CCITY436
-            
             Console.WriteLine("Welcome to BusBoard!");
-            
-            Console.WriteLine("Write stop code: ");
-            string stopCode = Console.ReadLine();
-            var u = "https://transportapi.com/v3/uk/bus/stop/"+ stopCode + "///timetable.json?";
-            using var client = new HttpClient();
+            Console.WriteLine("Enter a postcode: ");
+            string postCode = Console.ReadLine();
 
-            var builder = new UriBuilder(u);
-            builder.Query = "app_id=7d0ebcfd&app_key=bb3b6e2b09788d7ca770cd28ba1156bf&group=route&limit=5";
-            var url = builder.ToString();
-            var input =
-                await client.GetStringAsync(url);
+            string urlPost = "https://api.postcodes.io/postcodes/" + postCode;
             
+            
+            using var clientPost = new HttpClient();
+            var inputPost =
+                await clientPost.GetStringAsync(urlPost);
+
             //Console.WriteLine(result);
             
+            JObject jOresultPost = JObject.Parse(inputPost);
+            JToken dataPost = jOresultPost["result"];
+            Postcodes latLong = dataPost.ToObject<Postcodes>();
             
-            JObject jOresult = JObject.Parse(input);
+            var uLatLong = "http://transportapi.com/v3/uk/places.json?";
+            var builder = new UriBuilder(uLatLong);
+            builder.Query = "lat=" + latLong.latitude + "&lon=" + latLong.longitude + "&type=bus_stop&app_id=7d0ebcfd&app_key=bb3b6e2b09788d7ca770cd28ba1156bf&limit=2";
+            var urlLatLong = builder.ToString();
+            using var clientLatLong = new HttpClient();
+            var inputLatLong =
+                await clientPost.GetStringAsync(urlLatLong);
 
-            IList<JToken> data = jOresult["departures"]["905"].Children().ToList();
-            
-            IList<buses> busList = new List<buses>();
-
-            foreach (JToken bus in data)
+            JObject jOresultLatLong = JObject.Parse(inputLatLong);
+            IList<JToken> dataLatLong = jOresultLatLong["member"].Children().ToList();
+            IList<BusStops> stopList = new List<BusStops>();
+            foreach (JToken stop in dataLatLong)
             {
-                buses tempBus = bus.ToObject<buses>();
-                busList.Add(tempBus);
+                BusStops tempStop = stop.ToObject<BusStops>();
+                stopList.Add(tempStop);
             }
             
+            Console.WriteLine(stopList[0].atcocode);
+            
+            string url = CreateUrl(stopList[0].atcocode);
+            using var client = new HttpClient();
+            var input =
+                await client.GetStringAsync(url);
+
+
+            JObject jOresult = JObject.Parse(input);
+            List<JToken> routes = jOresult["departures"].Children().ToList();
+            List<List<JToken>> data = new List<List<JToken>>();   
+            foreach (JToken route in routes)
+            {
+                List<JToken> newRoute = routes.Children().ToList();
+                data.Add(newRoute);
+            }
+           
+            IList<Buses> busList = new List<Buses>();
+            foreach (List<JToken> busRoute in data){
+                foreach (JToken bus in busRoute)
+                {
+                    Buses tempBus = bus.ToObject<Buses>();
+                    busList.Add(tempBus);
+                }
+                
+            }
+            
+            //0500CCITY436
             Console.WriteLine("The next five buses are: ");
 
             for (int i = 0; i < busList.Count; i++)
@@ -45,12 +77,32 @@ namespace BusBoard
             }
 
         }
+        
+        public static string CreateUrl(string stopCode)
+        {
+            var u = "https://transportapi.com/v3/uk/bus/stop/"+ stopCode + "///timetable.json?";
+            var builder = new UriBuilder(u);
+            builder.Query = "app_id=7d0ebcfd&app_key=bb3b6e2b09788d7ca770cd28ba1156bf&group=route&limit=5";
+            var url = builder.ToString();
+            return url;
+        }
     }
 
-    public class buses
+    public class Buses
     {
         public string aimed_departure_time { get; set; }
         public string line { get; set; }
         public string direction { get; set; }
+    }
+
+    public class Postcodes
+    {
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+    }
+
+    public class BusStops
+    {
+        public string atcocode { get; set; }
     }
 }
